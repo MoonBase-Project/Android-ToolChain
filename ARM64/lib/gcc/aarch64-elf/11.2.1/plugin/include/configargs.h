388d290555b5ed22acce4ea73f752c11c862024f
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2022-02-04 18:11:35 -0400

    Android-ToolChain: Update |MoonBase|™ Gnu/ToolChain
    aarch64-elf & arm-eabi & x86-elf
    Debug Symbols Stripped-Out Binaries
    
    gcc version 11.2.1 20220204 (MoonBase-Android-ToolChain)
    GNU ld (jsX-Custom Gnu/Binutils) 2.38.50.20220204
    
    - Switch Binutils to master branch
    
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

---
+++ ARM64/lib/gcc/aarch64-elf/11.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --prefix=/home/jsx/GCC/G/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --with-zstd --enable-lto --with-isl --enable-threads=posix --disable-isl-version-check --enable-default-ssp --enable-languages=c --with-pkgversion=MoonBase-Android-ToolChain --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
